a525c06f9ef94563fa2a45ddd13b423c6b0b8308
Yoav BD <yoav.bendor1@mail.huji.ac.il>
2020-11-24 13:01:44 +0200

    first functioning triplot

--- soil_analyzer/grain_size/server.R
+++ soil_analyzer/grain_size/server.R
@@ -8,20 +8,167 @@
 #
 
 library(shiny)
+#library(data.table)
+library(RColorBrewer)
+library(extrafont)
+#library(rapport)
+library(shinyWidgets)
+#library(pracma)
+library(grDevices)
+library(Cairo)
+library(svglite)
+library(ggtern)
+library(plotly)
 library(tidyverse)
+library(ggplot2)
+
+#shinyOptions(shiny.maxRequestSize=1000*1024^2)
+options(shiny.maxRequestSize=1000*1024^2)
+
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+shinyServer(function(input, output, session) {
+    
+    inFile <- reactive({
+        if (is.null(input$input_file)) {
+            return(NULL)
+        } else {
+            input$input_file
+        }
     })
-
+    
+    
+    myData <- reactive({
+        if (is.null(inFile())) {
+            return(NULL)
+        } else {
+            
+            tmp_table = read_csv(inFile()$datapath)
+            # tmp_table = read_csv('Fazael_grain_size.csv')
+            
+            big_table=tmp_table
+            
+            # modify the factor columns into factors
+            big_table$sample_name <- factor(big_table$sample_name)
+            big_table$site <- factor(big_table$site)
+            big_table$fraction <- factor(big_table$fraction)
+            
+            # remove NA rows
+            big_table=big_table[!is.na(big_table$sample_name),]
+            
+            # make sure the table was loaded correctly
+            #  output$table <- renderTable((big_table))
+            
+            return(big_table)
+        }
+    })
+    
+    
+    
+    observe({
+        updatePickerInput(
+            session,
+            "color_variable",
+            choices = c('none',names(myData())[1:3]),
+            selected = 'none')
+    })
+    
+    observe({
+        updatePickerInput(
+            session,
+            "selected_site",
+            choices = levels(myData()$site),
+            selected = levels(myData()$site[1:length(myData()$site)]))
+    })
+    
+    observe({
+        updatePickerInput(
+            session,
+            "selected_sample_name",
+            choices = levels(myData()$sample_name),
+            selected = levels(myData()$sample_name[1:length(myData()$sample_name)]))
+    })
+    
+    observe({
+        updatePickerInput(
+            session,
+            "selected_fraction",
+            choices = levels(myData()$fraction),
+            selected = levels(myData()$fraction[1:length(myData()$fraction)]))
+    })
+    
+    myPlot <- function(){
+        # make sure the data exists
+        req(myData())
+        
+        big_table=myData()
+        
+        # output$table <- renderTable(big_table)
+        
+        # filter the data based on user selection
+        big_table = filter(big_table,sample_name %in% input$selected_sample_name)
+        big_table = filter(big_table,site %in% input$selected_site)
+        big_table = filter(big_table,fraction %in% input$selected_fraction)
+        
+        p =  big_table %>% 
+            ggtern(aes_string(x=colnames(big_table)[4],y=colnames(big_table)[5],z=colnames(big_table)[6])) +
+            geom_point(size=input$point_size)  
+            # scale_color_brewer(palette = "Paired")
+        
+        if (!(input$color_variable %in% c("none"))) {
+            p = p +
+                geom_point(aes_string(color=input$color_variable))
+        }
+        
+        p = p+  
+            theme_light()
+        
+        if (input$add_color_theme) {
+            p = p+  
+                theme_rgbw()
+        } 
+        
+        p+
+            theme(
+                #axis.title=element_text(size=14),
+                axis.text=element_text(size=14))+
+            xlab(input$x_title)+
+            ylab(input$y_title)+
+            zlab(input$z_title)
+        
+    }
+    
+    
+    output$plot1 <- renderPlot({
+        print(myPlot())
+    })
+    
+    
+    output$download_plot_png <- downloadHandler(
+        filename = function() { paste(input$figure_name, '.png', sep='')},
+        
+        content = function(file) {
+            ggsave(file,plot = myPlot(), device = "png",width = input$figure_width, height = input$figure_height, units = "cm",limitsize = FALSE,dpi = input$figure_dpi)
+        }
+    )
+    
+    output$download_plot_pdf <- 
+        downloadHandler(
+            filename = function(){ paste(input$figure_name,'.pdf', sep='')},
+            content = function(file) {
+                ggsave(file,plot = myPlot(), device = cairo_pdf(),width = input$figure_width, height = input$figure_height, dpi = input$figure_dpi, units = "cm")
+            }
+        ) 
+    
+    output$download_plot_svg <- 
+        downloadHandler(
+            filename = function(){
+                paste(input$figure_name,'.svg', sep='')
+            },
+            
+            content = function(file) {
+                ggsave(file,plot = myPlot(), device = svg,width = input$figure_width, height = input$figure_height, dpi = input$figure_dpi, units = "cm")
+            }
+        )  
+    
 })
--- soil_analyzer/grain_size/ui.R
+++ soil_analyzer/grain_size/ui.R
@@ -8,26 +8,132 @@
 #
 
 library(shiny)
+library(shinyWidgets)
+#library(periscope)
+
+# shinyOptions(shiny.maxRequestSize=1000*1024^2)
+options(shiny.maxRequestSize=1000*1024^2)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
+    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
+    titlePanel("Grain Size Analysis"),
+    
+    plotOutput("plot1",
+               width = "1200px",
+               height = "400px"),
+    
+    hr(),
+    
     # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+    fluidRow(
+        column(width = 3,
+               offset = 0,
+               h4('load and plot'),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+               fileInput(inputId='input_file', 
+                         label = h5('upload files for analyses'),
+                         multiple = TRUE
+               ),
+
+               
+               pickerInput(
+                   inputId = "color_variable",
+                   label = "Select color variable", 
+                   choices = "",
+                   multiple = FALSE
+               ),
+               
+               materialSwitch(
+                   inputId = "add_color_theme",
+                   label = "color theme", 
+                   status = "primary",
+                   right = TRUE
+               )
+               
+
+        ),
+               
+            
+        
+        column(width = 2,
+               offset = 0,
+               h4('data to plot'),
+               
+               pickerInput(inputId="selected_site",
+                           label = h5("sites to plot"),
+                           choices="",
+                           options = list(`actions-box` = TRUE),multiple = T),
+
+               pickerInput(inputId="selected_sample_name",
+                           label = h5("select samples"),
+                           choices="",
+                           options = list(`actions-box` = TRUE),multiple = T),
+               
+               pickerInput(inputId="selected_fraction",
+                           label = h5("select fraction"),
+                           choices="",
+                           options = list(`actions-box` = TRUE),multiple = T),
+        ),
+        
+        
+        column(width = 2,
+               offset = 0,
+               h4('plot appearance'),
+               
+               textInput(inputId="x_title",
+                         label = h5("x title"),
+                         value="125um < x < 2mm"),
+               
+               textInput(inputId="y_title",
+                         label = h5("y title"),
+                         value="63um < x < 125um"),
+               
+               textInput(inputId="z_title",
+                         label = h5("z title"),
+                         value="63um < x"),
+               
+               sliderInput(inputId = "point_size",
+                           label = h5("point size"),
+                           min=0, max=10,
+                           value=1, step=0.25)
+
+        ),
+        
+        column(width = 3,
+               offset = 0,
+               h4('download figure'),
+               
+               sliderInput(inputId="figure_height",
+                           label = h5("set figure height [cm]"),
+                           min=0,max=30,
+                           value=10, step=1),
+               
+               sliderInput(inputId="figure_width",
+                           label = h5("set figure width [cm]"),
+                           min=0,max=30,
+                           value=20, step=1),
+               
+               sliderInput(inputId="figure_dpi",
+                           label = h5("set dpi"),
+                           min=0,max=1000,
+                           value=300, step=20),
+               
+               textInput(inputId="figure_name",
+                         label = h5("figure name"),
+                         value="triplot fig"),
+
+               downloadButton(outputId='download_plot_png', label='Download PNG Figure'),
+               
+               downloadButton(outputId='download_plot_pdf', label='Download PDF Figure'),
+               
+               downloadButton(outputId='download_plot_svg', label='Download SVG Figure')
+               
+              
+        ),
     )
-))
+    
+)
+
+)
